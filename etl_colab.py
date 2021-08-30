4135147789f63ccd5450e549996320d1810870e5
yemauricio <yemauricio@users.noreply.github.com>
2021-08-30 16:11:16 -0500

    Update etl_colab.py

--- etl_colab.py
+++ etl_colab.py
@@ -22,20 +22,31 @@ def clean_str(string):
     string_new = string.replace("¿","").replace("?","").lstrip().rstrip().replace(",","").                replace("á","a").replace("é","e").replace("í","i").replace("ó","o").replace("ú","u").lower().                replace("¡","").replace("!","").replace(".","")
     return string_new
 
+def read_files():
+    calf = pd.read_excel(r'/content/caramelo/insumo/1.calificacion.xlsx', sheet_name=0)
+    demo = pd.read_excel(r'/content/caramelo/insumo/2.demografica.xlsx',  sheet_name=0)
+    return calf, demo
+
 def drop_duplicates():
-    file_ins = glob.glob('insumo/*.xlsx')
-    cols_cal = list(pd.read_excel("/content/caramelo/catalogos/1.catalogo_1.xlsx")["Identificador_pregunta"].values)
+    file_ins = r'/content/caramelo/insumo/1.calificacion.xlsx'
+    file_dem = r'/content/caramelo/insumo/2.demografica.xlsx'
+    cols_cal = list(
+              pd.read_excel(
+                r"/content/caramelo/catalogos/1.catalogo_1.xlsx"
+                )["Identificador_pregunta"].values)
     cols_cal.append("#")
-    insumo = pd.read_excel(file_ins[1],sheet_name=0)
-    cols_cal =  [i for i in insumo.columns if i in cols_cal]
-    demo = pd.read_excel(file_ins[0],sheet_name=0)
+    insumo = pd.read_excel(file_ins, sheet_name=0)
+    # cols_cal =  [i for i in insumo.columns if i in cols_cal]
+    demo = pd.read_excel(file_dem, sheet_name=0)
 
     print("borrando duplicados en calificacion")
     insumo.drop_duplicates(subset=["#"], inplace=True)
-    insumo[cols_cal].set_index("#").to_excel(file_ins[1])
+    insumo = insumo.set_index("#")
+    insumo.to_excel(file_ins)
     print("borrando duplicados en demograficos")
     demo.drop_duplicates(subset=["index"], inplace=True)
-    demo.set_index("index").to_excel(file_ins[0])
+    demo = demo.set_index("index")
+    demo.to_excel(file_dem)
     del insumo
     del demo
 
@@ -45,12 +56,12 @@ def extract_cod():
     funcion que extrae el codigo de la pregunta
     """
     drop_duplicates()
-    file_ins = glob.glob('insumo/*.xlsx')
-    insumo = pd.read_excel(file_ins[1],sheet_name=0)
+    file_ins = glob.glob(r'/content/caramelo/insumo/*.xlsx')
+    insumo = read_files()[0]
     df_temp = insumo.T.reset_index()
     df_temp["index"] = df_temp.apply(lambda row: row["index"].split()[0] ,axis=1)
     df_temp = df_temp.set_index("index").T.reset_index(drop=True).set_index("#")
-    df_temp.to_excel(file_ins[1])
+    df_temp.to_excel(r'/content/caramelo/insumo/1.calificacion.xlsx')
 
 def cramers_corrected_stat(x,y):
 
@@ -81,8 +92,9 @@ def cramers_corrected_stat(x,y):
             kcorr = k - ((k-1)**2)/(n-1)
             result=np.sqrt(phi2corr / min( (kcorr-1), (rcorr-1)))
         return round(result,6)
+        
 def genera_case():
-    file_res = glob.glob('resultado/correlacion_variables.csv')
+    file_res = glob.glob(r'/content/caramelo/resultado/correlacion_variables.csv')
     punt = pd.read_csv(file_res[0], sep=";")["vars1"].unique()
     not_in = ['estado civil','educacion','antigüedad en el cargo','nivel del cargo', 'hijos'
               ,'genero','area','antigüedad en la organización',"edad"]
@@ -95,7 +107,7 @@ def genera_case():
 
     stringfinal1 = "ELSE '' \n"
     stringfinal2 = "END"
-    with open("resultado/case_preguntas.txt", 'w') as x_file:
+    with open(r"/content/caramelo/resultado/case_preguntas.txt", 'w') as x_file:
         x_file.write(string1)
         for i in range(len(listadomin)):
             n = stringn.format(listadomin[i],listadomin[i])
@@ -104,7 +116,7 @@ def genera_case():
         x_file.write(stringfinal2)
         
 def genera_case_actuacion():
-    file_res = glob.glob('resultado/correlacion_variables.csv')
+    file_res = glob.glob(r'/content/caramelo/resultado/correlacion_variables.csv')
     punt = pd.read_csv(file_res[0], sep=";")["vars1"].unique()
     actuaciones = ['Conexion',
                 'Equilibrio',
@@ -133,7 +145,7 @@ def genera_case_actuacion():
     stringn = "WHEN '{0}'  THEN avg([{1}]) \n"
 
     stringfinal2 = "END"
-    with open("resultado/case_actuaciones.txt", 'w') as x_file:
+    with open(r"/content/caramelo/resultado/case_actuaciones.txt", 'w') as x_file:
         x_file.write(string1)
         for i in range(len(actuaciones)):
             n = stringn.format(actuaciones[i],actuaciones[i])
@@ -146,20 +158,18 @@ def genera_case_actuacion():
 
 def tabla_insumo_agg():
     # Leer archivos de insumos y catalogo
-    file_cat = glob.glob('catalogos/*.xlsx')
-    file_ins = glob.glob('insumo/*.xlsx')
-    print(file_ins)
-    print(file_cat)
+    file_cat = glob.glob(r'/content/caramelo/catalogos/*.xlsx')
+    file_ins = glob.glob(r'/content/caramelo/insumo/*.xlsx')
 
     # Catalogo de respuestas
-
-    inverso = file_cat[1] #catalogo_1.xlsx"
-    puntaje = file_cat[0] #catalogo_2.xlsx"
+    inverso = r"catalogos/1.catalogo_1.xlsx" #catalogo_1.xlsx"
+    puntaje = r"catalogos/2.catalogo_2.xlsx" #catalogo_2.xlsx"
 
     # Leer archivos
     catalogo_punt = pd.read_excel(puntaje)
     catalogo_inverso= pd.read_excel(inverso)
-    insumo = pd.read_excel(file_ins[1]).set_index("#")
+    insumo = pd.read_excel(r'/content/caramelo/insumo/1.calificacion.xlsx'
+                          , sheet_name=0).set_index("#")
 
     # Limpiar el valor de las respuestas de signos
     catalogo_punt["Respuestas"] = catalogo_punt.apply(lambda row: str(row["Respuestas"]), axis=1) 
@@ -213,7 +223,6 @@ def tabla_insumo_agg():
     df_t = df_total_pnt[cols].T
     cols2 = df_t.columns[:-2]
     invers = {1:5,2:4,3:3,4:2,5:1}
-    print(df_t)
     for c in cols2:
       df_t[c] = df_t.apply(lambda row: row[c] if row["Inversa"] == "0" else invers[row[c]] , axis=1)
     df_tabla_final = df_t.T
@@ -269,7 +278,7 @@ def tabla_insumo_agg():
     # formatear columnas que se exportaran
     for cols_to_form in [1,2,3,4,5,"prom","sum"]:
         df_agg[cols_to_form] = df_agg.apply(lambda row: str(row[cols_to_form]).replace(".",","), axis=1)
-    df_agg.to_csv("resultado/resultado.csv",sep=";")
+    df_agg.to_csv(r"/content/caramelo/resultado/resultado.csv",sep=";")
     #     return df_agg
 
 
@@ -279,12 +288,12 @@ def tabla_insumo_agg():
 
 def demografico():
     # Leer resultado de id_puntajes 
-    file_ins = glob.glob('insumo/*.xlsx')
-    file_res = glob.glob('resultado/id_puntajes.csv')
-    demo = pd.read_excel(file_ins[0]).set_index("index")
+    file_ins = glob.glob(r'/content/caramelo/insumo/*.xlsx')
+    file_res = glob.glob(r'/content/caramelo/resultado/id_puntajes.csv')
+    demo = pd.read_excel(r'/content/caramelo/insumo/2.demografica.xlsx').set_index("index")
     insumo_punt = pd.read_csv(file_res[0]).set_index("index")
     df_corr = insumo_punt.merge(demo, left_index=True, right_index=True)
-    df_corr.to_csv('resultado/id_puntajes.csv')
+    df_corr.to_csv(r'/content/caramelo/resultado/id_puntajes.csv')
 
     # categorizacion de las columnas demograficas
     for i in demo.columns:
@@ -304,21 +313,28 @@ def demografico():
           if col_demo != col_punt:
               if ((col_punt,col_demo) not in combinaciones) :
                   combinaciones.append((col_demo, col_punt))
-                  temp = pd.DataFrame({'vars1':[col_demo]
-                                      ,"vars2":[col_punt]
-                                      ,"cramer_corr":[cramers_corrected_stat(df_corr[col_demo],df_corr[col_punt])]})
+                  try:
+                    temp = pd.DataFrame({'vars1':[col_demo]
+                                        ,"vars2":[col_punt]
+                                        ,"cramer_corr":[cramers_corrected_stat(df_corr[col_demo],df_corr[col_punt])]})
+                  except:
+                    
+                    temp = pd.DataFrame({'vars1':[col_demo]
+                                        ,"vars2":[col_punt]
+                                        ,"cramer_corr":[0]})
+
                   df_corr_cramer = df_corr_cramer.append(temp)
 
     df_corr_vars = df_corr_cramer.sort_values("cramer_corr",ascending=False)
 
-    file_cat = glob.glob('catalogos/*.xlsx')
-    inverso = file_cat[1]
-    catalogo_inverso= pd.read_excel(inverso)
+    # file_cat = glob.glob(.xlsx')
+    # inverso = file_cat[1]
+    catalogo_inverso= pd.read_excel(r'/content/caramelo/catalogos/1.catalogo_1.xlsx', sheet_name=0)
     df_corr = df_corr_vars.merge(catalogo_inverso[["Identificador_pregunta","actuacion","nivel"]]
                                  , left_on="vars2"
                                  ,right_on="Identificador_pregunta")[["vars1","vars2","cramer_corr","actuacion","nivel","Identificador_pregunta"]]
     df_corr["cramer_corr"] = df_corr.apply(lambda row: str(row["cramer_corr"]).replace(".",","), axis=1)
-    df_corr.to_csv("resultado/correlacion_variables.csv",sep=";")
+    df_corr.to_csv(r"/content/caramelo/resultado/correlacion_variables.csv",sep=";")
     # #     return df_corr_vars
 
 
@@ -329,13 +345,13 @@ def aggregado_id():
     """
     Funcion que ejecuta la agregación por actuacion  global
     """
-    file_res = glob.glob('resultado/id_puntajes.csv')  
-    file_act_glo = glob.glob("resultado/actuacion_global.csv")
+    file_res = glob.glob(r'/content/caramelo/resultado/id_puntajes.csv')  
+    file_act_glo = glob.glob(r"/content/caramelo/resultado/actuacion_global.csv")
     punt = pd.read_csv(file_res[0])#.rename(columns={"Unnamed: 0":"id"}).set_index("id")
     act_glo = pd.read_csv(file_act_glo[0])
     temp1 = punt.T.merge(act_glo, left_index=True,right_on="Identificador_pregunta", how="left")
     temp1 = temp1.set_index("Identificador_pregunta").drop(columns="Unnamed: 0").T.to_csv("resultado/id_puntajes.csv")
-    os.remove('resultado/actuacion_global.csv')
+    os.remove(r'/content/caramelo/resultado/actuacion_global.csv')
     punt = pd.read_csv(file_res[0]).rename(columns={"Unnamed: 0":"id"}).set_index("id")
     temp = punt.T
     temp.drop(columns="actuacion global",inplace=True)
@@ -401,10 +417,10 @@ def aggregado_id():
         df_final_act[i] = df_final_act[[i]].apply(lambda row: row[i].astype("str").replace(".",","), axis=1)
       except:
         continue
-    df_final_act.to_csv("resultado/id_puntajes.csv")
+    df_final_act.to_csv(r"/content/caramelo/resultado/id_puntajes.csv")
 
 def limpieza_final():
-    punt = r"resultado/id_puntajes.csv"
+    punt = r"/content/caramelo/resultado/id_puntajes.csv"
     df_punt = pd.read_csv(punt).reset_index(drop=True).rename(columns={"id":"index","index":"id"})
     df_punt.to_csv(punt)
 
